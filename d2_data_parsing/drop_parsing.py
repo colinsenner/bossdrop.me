c1a71d95241b7742586b3d019446fa7f325ca09c
Colin Senner <colin.senner@gmail.com>
2021-11-23 22:27:31 -0800

    Write test code which can fetch all TCs for a given boss

---
+++ d2_data_parsing/drop_parsing.py
@@ -0,0 +1,105 @@
+import os
+from collections import deque
+
+import pandas as pd
+
+import common
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+treasureClassEx = common.create_dictionary(os.path.join(data_dir, "TreasureClassEx.txt"))
+
+def main():
+    boss = 'Duriel'
+    boss_tcs = AllTreasureClasses('Duriel')
+
+    print(f"{boss} - TCs")
+    print("--------------")
+    print(boss_tcs)
+
+
+def GetSubTreasureClasses(treasure_class_name):
+    """Returns all sub treasure classes from a treasure_class_name
+    e.g.
+        For 'Duriel' returns ['tsc', 'Duriel - Base']
+
+    TreasureClassEx.txt
+        Treasure Class ... Item1 ... Item2         ... Item3
+        Duriel             tsc       Duriel - Base     NaN
+
+    Args:
+        treasure_class_name ([str]): name of the row in the column 'Treasure Class'
+
+    Returns:
+        [str]: List of all Item1-10 strings that aren't NaN for the matching row
+    """
+    treasure_classes = []
+
+    for row in treasureClassEx:
+        if row['Treasure Class'] == treasure_class_name:
+            # Go through item1 - item10
+            item_columns = [f"Item{i}" for i in range(1,11)]
+
+            for col in item_columns:
+                tc = row[col]
+
+                # Pandas columns without data return NaN
+                if not pd.isna(tc):
+                    treasure_classes.append(tc)
+
+    return treasure_classes
+
+
+def AllTreasureClasses(treasure_class_name):
+    """Returns every droppable Treasure Class for a given Treasure Class
+
+    Args:
+        treasure_class_name ([str]): name of the row in the column 'Treasure Class'
+
+    Returns:
+        [str]: List of every Treasure Class including all sub TCs for a given string
+    """
+    q = deque([treasure_class_name])
+    all_treasure_classes = []
+
+    while q:
+        entry = q.popleft()
+
+        # Get the column 'monster' from TreasureClassEx.txt and find all Item1-10 entries
+        # Add them to our queue
+        for tc in GetSubTreasureClasses(entry):
+            q.append(tc)
+
+        all_treasure_classes.append(entry)
+    return all_treasure_classes
+
+
+def AllTreasureClassesRecursive(treasure_class_name, all_treasure_classes):
+    """Returns every droppable Treasure Class for a given Treasure Class
+
+    Args:
+        treasure_class_name ([str]): name of the row in the column 'Treasure Class'
+
+    Returns:
+        [str]: List of every Treasure Class including all sub TCs for a given string
+    """
+    all_treasure_classes.append(treasure_class_name)
+
+    # Base case
+    # Some values in 'Item#' are empty, pandas returns NaN
+    if pd.isna(treasure_class_name):
+        return
+
+    for row in treasureClassEx:
+        if row['Treasure Class'] == treasure_class_name:
+            # Go through columns Item1-10
+            item_columns = [f"Item{i}" for i in range(1,11)]
+
+            for col in item_columns:
+                entry = row[col]
+                AllTreasureClassesRecursive(entry, all_treasure_classes)
+    return all_treasure_classes
+
+
+if __name__ == '__main__':
+    main()
