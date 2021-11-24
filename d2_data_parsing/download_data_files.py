b8bf99da35d0eb1ea99678db4efe2ff36f5cf20f
Colin Senner <colin.senner@gmail.com>
2021-11-23 22:26:11 -0800

    Fixed line endings and encoding of downloaded files

--- d2_data_parsing/download_data_files.py
+++ d2_data_parsing/download_data_files.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 download_url = f'https://raw.githubusercontent.com/fabd/diablo2/master/code/d2_113_data/'
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-files_to_download = ['MonStats.txt', 'UniqueItems.txt', "Armor.txt"]
+files_to_download = ['MonStats.txt', 'UniqueItems.txt', 'Armor.txt', 'Weapons.txt', 'Misc.txt']
 
 os.makedirs(data_dir, exist_ok=True)
 
@@ -18,7 +18,7 @@ for file in files_to_download:
 
     assert res.status_code == 200, f"Problem downloading file: {file}"
 
-    with open(local_file, "wb") as f:
-        f.write(res.content)
+    with open(local_file, "wt", encoding='utf-8') as f:
+        f.write(res.text)
 
 print("Files downloaded.")
