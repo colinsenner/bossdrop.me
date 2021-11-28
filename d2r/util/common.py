0f8c81927f4e8c6f269eb8128bb0edab482260ef
Colin Senner <colin.senner@gmail.com>
2021-11-28 15:08:08 -0800

    parse items

--- d2r/util/common.py
+++ d2r/util/common.py
@@ -2,4 +2,4 @@ import os
 from os import path
 
 def get_data_dir():
-    return path.join(os.getcwd(), "d2r", "data")
+    return path.join(os.getcwd(), "data")
--- d2r/util/items.py
+++ d2r/util/items.py
@@ -11,57 +11,89 @@ def EquipGroupLevel(level):
     return level + (3 - (level % 3))
 
 
+def find_level(row, other_df):
+    base_item_level = row['base_item_level']
+
+    # Lookup the item level in the other dataframe
+    if pd.isna(base_item_level):
+        if row.code in other_df.index:
+            return other_df.loc[row.code].level
+    return base_item_level
+
+
+def get_base_tc_class(row):
+    pass
+
+
+def remove_duplicate_rows(df):
+    '''
+    Returns a dataframe with duplicate indexes removed
+    '''
+    return df[~df.index.duplicated(keep='first')]
+
+
 def get_unique_items():
-    df = pd.read_csv(os.path.join(get_data_dir(), 'UniqueItems.txt'), sep='\t')
+    #
+    # Read UniqueItems.txt and setup the dataframe to only include enabled items with codes
+    #
+    unique_items = pd.read_csv(os.path.join(get_data_dir(), 'UniqueItems.txt'), sep='\t')
+    unique_items.set_index('index', inplace=True, drop=True)
 
-    # Drop rows where the items aren't enabled
-    df = df[df['enabled'] == 1.0]
+    df1 = unique_items.copy()
 
-    # Drop rows where we have NaN is code column
-    df = df[df['code'].notna()]
+    df1 = df1[df1['enabled'] == 1.0]
+    df1 = df1[df1['code'].notna()]
+
+    # Add additional columns, we'll calculate these below
+    df1['base_item_level'] = np.nan
+    df1['base_tc_class'] = np.nan
 
     #
     # Read Weapons.txt to find the base item type's level
     #
-    df_weapons = pd.read_csv(os.path.join(get_data_dir(), 'Weapons.txt'), sep='\t')
-
-    keep_columns = ['name', 'type', 'code', 'level']
-    df = df.merge(df_weapons[keep_columns], how='left', on='code', validate='many_to_one')
+    weapons = pd.read_csv(os.path.join(get_data_dir(), 'Weapons.txt'), sep='\t')
+    weapons.set_index('code', inplace=True, drop=True, verify_integrity=True)
 
     #
     # Read Armor.txt to find the base item type's level
     #
-    df_armor = pd.read_csv(os.path.join(get_data_dir(), 'Armor.txt'), sep='\t')
-
-    df = df.merge(df_armor[keep_columns], how='left', on='code', validate='many_to_one')
-
-    print(df.head())
-    print()
-
-    # for index, row in df.iterrows():
-    #     item_name = row['index']
-
-    #     base_item = df_weapons.loc[df_weapons.code == row.code]
-
-    #     print(base_item['level'])
-
-    #     assert len(base_item) == 0 or len(base_item) == 1, f"Multiple items found in 'Weapons.txt' with code '{row.code}'"
-
-    #     # Not found in this file
-    #     if base_item.empty:
-    #         continue
-
-    #     # assert not base_item.empty, f"Couldn't find item code '{row.code}' for UniqueItem '{item_name}' in 'Weapons.txt'"
-
-    #     df.at[index, 'base_item_level'] = base_item['level']
-    #     df.at[index, 'base_tc_class'] = f"weap{base_item.iloc[0].level}"
+    armor = pd.read_csv(os.path.join(get_data_dir(), 'Armor.txt'), sep='\t')
+    armor.set_index('code', inplace=True, drop=True, verify_integrity=True)
 
     #
-    # Read Armor.txt to find the base item type's level
+    # Read Misc.txt to find the base item type's level
     #
+    misc = pd.read_csv(os.path.join(get_data_dir(), 'Misc.txt'), sep='\t')
+    misc.set_index('code', inplace=True, drop=True, verify_integrity=True)
 
+    #
+    # Add runes to our items list
+    #
+    # runes = misc[misc.name.str.contains("Rune")]
 
+    # # misc is indexed on 'code', so we need to assign it's code to another column
+    # # so it'll propogate when appended to the other df
+    # runes.loc[runes.index, 'code'] = runes.index
 
-    print(df.head(100))
+    # # Only keep these columns
+    # runes = runes[['name', 'code']]
+    # runes.set_index('name', inplace=True)
 
-    return df
+    # df1 = df1.append(runes)
+
+    #
+    # Calculate
+    # Lookup their base items levels
+    df1['base_item_level'] = df1.apply(find_level, args=(weapons,), axis=1)
+    df1['base_item_level'] = df1.apply(find_level, args=(armor,), axis=1)
+    df1['base_item_level'] = df1.apply(find_level, args=(misc,), axis=1)
+
+    assert df1['base_item_level'].isnull().sum() == 0, f"One or more of the base_item_levels are null"
+
+    # Assign their base TC class
+    df1['base_tc_class'] = df1.apply(get_base_tc_class, axis=1)
+
+    # 'Rainbow Facet' - shows up multiple times, they're all the same level with the same code, remove them
+    df1 = remove_duplicate_rows(df1)
+
+    return df1
