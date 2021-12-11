2c4e772c91260c1b8d8c2deee0fd3625ba6231ec
Kit Allen <kit@clevero.co>
2021-12-11 18:48:33 +1100

    Started working on a dark theme; currently no toggle button implemented

--- src/static/d2r.js
+++ src/static/d2r.js
@@ -1,11 +1,10 @@
-var d2data = {}
+var d2data = {};
 
-$.getJSON('results.json', function(data) {
+$.getJSON("results.json", function (data) {
     d2data = data;
 
-    console.log("Huge thanks to Goretusk and Sinny!")
+    console.log("Huge thanks to Goretusk and Sinny!");
 });
-
 
 function searchresult_onclick(data) {
     let search_input = document.getElementById("search");
@@ -13,14 +12,15 @@ function searchresult_onclick(data) {
 
     clear_search_results();
     search_onsubmit();
-};
+}
 
 function clear_search_results() {
-    removeAllChildNodes(document.querySelector('#search_results'));
+    document.querySelector("#search_results").classList.remove("has-items");
+    removeAllChildNodes(document.querySelector("#search_results"));
 }
 
 function clear_results() {
-    removeAllChildNodes(document.querySelector('#boss_results'));
+    removeAllChildNodes(document.querySelector("#boss_results"));
 }
 
 function search_onsubmit() {
@@ -30,9 +30,9 @@ function search_onsubmit() {
     var bosses = get_bosses_that_can_drop(search.value);
 
     // Sort bosses based on their normal level
-    bosses.sort((a, b) => a['Level'] - b['Level']);
+    bosses.sort((a, b) => a["Level"] - b["Level"]);
 
-    results.innerHTML = '';
+    results.innerHTML = "";
 
     if (bosses.length > 0) {
         showDisclaimer();
@@ -44,22 +44,20 @@ function search_onsubmit() {
             <div class="header">Hell</div>
         `;
 
-        bosses.forEach((boss) => {
-            if (boss['difficulty'].some(x => x)) {
-                let row = '';
+        bosses.forEach(boss => {
+            if (boss["difficulty"].some(x => x)) {
+                let row = "";
 
-                row += `<div class="boss_name">${boss['name']}</div>`
+                row += `<div class="boss_name">${boss["name"]}</div>`;
 
                 for (let index = 0; index < 3; index++) {
-                    if (boss['difficulty'][index])
-                        row += `<div>✔️</div>`
-                    else
-                        row += `<div class="opacity-20">✖</div>`
+                    if (boss["difficulty"][index]) row += `<div>✔️</div>`;
+                    else row += `<div class="opacity-20">✖</div>`;
                 }
 
                 table_header += row;
             }
-        })
+        });
 
         results.innerHTML = table_header;
     } else {
@@ -80,49 +78,52 @@ function hideDisclaimer() {
 }
 
 function get_bosses_that_can_drop(item_name) {
-    let all_monsters = d2data['monsters']
-    let all_items = d2data['items'];
+    let all_monsters = d2data["monsters"];
+    let all_items = d2data["items"];
 
-    let item = all_items.filter(item => item['index'] == item_name)[0];
+    let item = all_items.filter(item => item["index"] == item_name)[0];
 
     if (item !== undefined) {
         // Found the item
-        let bosses = []
+        let bosses = [];
 
-        all_monsters.forEach((boss) => {
-
+        all_monsters.forEach(boss => {
             let boss_entry = {
-                "name": boss['NameStr'],
-                "Level": boss['Level'],
-                "Level(N)": boss['Level(N)'],
-                "Level(H)": boss['Level(H)'],
-                "difficulty": [false, false, false]
+                name: boss["NameStr"],
+                Level: boss["Level"],
+                "Level(N)": boss["Level(N)"],
+                "Level(H)": boss["Level(H)"],
+                difficulty: [false, false, false],
             };
 
-            ['', '(N)', '(H)'].forEach((difficulty) => {
-                difficulty_str = {  '':'Normal',
-                                    '(N)':'Nightmare',
-                                    '(H)': 'Hell'
-                                }
+            ["", "(N)", "(H)"].forEach(difficulty => {
+                difficulty_str = {
+                    "": "Normal",
+                    "(N)": "Nightmare",
+                    "(H)": "Hell",
+                };
                 let difficult_name = difficulty_str[difficulty];
 
-                let boss_level = boss['Level' + difficulty];
-                let boss_dropped_tcs = boss['TC' + difficulty];
-                let item_tc_group = item['tc_group'];
+                let boss_level = boss["Level" + difficulty];
+                let boss_dropped_tcs = boss["TC" + difficulty];
+                let item_tc_group = item["tc_group"];
 
-                let boss_is_high_enough_level = (boss_level >= item.lvl);
-                let boss_can_drop_that_treasure_class = boss_dropped_tcs.includes(item_tc_group);
+                let boss_is_high_enough_level = boss_level >= item.lvl;
+                let boss_can_drop_that_treasure_class =
+                    boss_dropped_tcs.includes(item_tc_group);
 
-                if (boss_is_high_enough_level && boss_can_drop_that_treasure_class)
-                {
-                    if (difficult_name === 'Normal') {
-                        boss_entry['difficulty'][0] = true;
+                if (
+                    boss_is_high_enough_level &&
+                    boss_can_drop_that_treasure_class
+                ) {
+                    if (difficult_name === "Normal") {
+                        boss_entry["difficulty"][0] = true;
                     }
-                    if (difficult_name === 'Nightmare') {
-                        boss_entry['difficulty'][1] = true;
+                    if (difficult_name === "Nightmare") {
+                        boss_entry["difficulty"][1] = true;
                     }
-                    if (difficult_name === 'Hell') {
-                        boss_entry['difficulty'][2] = true;
+                    if (difficult_name === "Hell") {
+                        boss_entry["difficulty"][2] = true;
                     }
                 }
             });
@@ -139,16 +140,16 @@ function get_bosses_that_can_drop(item_name) {
 }
 
 function autocompleteMatch(input) {
-    if (input == '') {
+    if (input == "") {
         return [];
     }
-    var reg = new RegExp(input, "i")
+    var reg = new RegExp(input, "i");
 
-    let items = d2data['items'].map(x => x['index']);
+    let items = d2data["items"].map(x => x["index"]);
 
-    return items.filter(function(term) {
+    return items.filter(function (term) {
         if (term.match(reg)) {
-        return term;
+            return term;
         }
     });
 }
@@ -156,26 +157,33 @@ function autocompleteMatch(input) {
 function searchFocus() {
     let search_box = document.getElementById("search");
 
-    if (search_box !== undefined)
-        search_box.value = "";
+    if (search_box !== undefined) search_box.value = "";
 }
 
 function showSearchResults(val) {
     res = document.getElementById("search_results");
-    res.innerHTML = '';
-    let list = '';
+    res.innerHTML = "";
+    let list = "";
     let terms = autocompleteMatch(val);
 
     if (terms.length !== 0) {
-        for (i=0; i < terms.length; i++) {
-            list += '<li tabindex="0" onkeypress="searchresult_onclick(this.innerHTML)" onclick="searchresult_onclick(this.innerHTML)">' + terms[i] + '</li>';
+        for (i = 0; i < terms.length; i++) {
+            list +=
+                '<li tabindex="0" onkeypress="searchresult_onclick(this.innerHTML)" onclick="searchresult_onclick(this.innerHTML)">' +
+                terms[i] +
+                "</li>";
         }
-    }
-    else if (val !== '') {
+    } else if (val !== "") {
         list += '<li tabindex="0">Item not found</li>';
     }
 
-    res.innerHTML = '<ul>' + list + '</ul>';
+    res.innerHTML = "<ul>" + list + "</ul>";
+
+    if (document.getElementsByTagName("li")) {
+        res.classList.add("has-items");
+    } else {
+        res.classList.remove("has-items");
+    }
 
     search_onsubmit();
     return false;
@@ -185,4 +193,5 @@ function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/**/*.html',
-    './src/**/*.njk',
-    './src/**/*.md',
-    './src/**/*.11ty.js'
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['Montserrat', 'Open Sans', 'sans-serif'],
-      serif: ['exocet', 'Merriweather', 'serif'],
+    mode: "jit",
+    purge: [
+        "./src/**/*.html",
+        "./src/**/*.njk",
+        "./src/**/*.md",
+        "./src/**/*.11ty.js",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Montserrat", "Open Sans", "sans-serif"],
+            serif: ["exocet", "Merriweather", "serif"],
+        },
+        extend: {
+            colors: {
+                "diablo-dark": "#1c1d21",
+                "diablo-darker": "#17181c",
+                "diablo-darkest": "#0c0c0e",
+                "diablo-light": "#f3f4f6",
+                "diablo-yellow": "#B79A64",
+            },
+        },
+        boxShadow: {
+            "diablo-input": "0 0 0 4px rgba(183,154,100,0.3)"
+        }
     },
-  },
-}
+    darkMode: "class",
+};
