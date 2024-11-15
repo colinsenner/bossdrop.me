56a030683b202ae38242d8467940cfa438749466
Colin Senner <colin.senner@gmail.com>
2024-11-14 20:57:55 -0800

    update: parsing code to 2.7.0 file formats

--- d2r/newutil/bosses.py
+++ d2r/newutil/bosses.py
@@ -6,6 +6,7 @@ import pandas as pd
 from .common import get_base_file, get_generated_dir
 from .treasureclasses import get_treasure_class_for_difficulty, get_all_treasure_classes
 
+
 def get(version):
     monstats = get_base_file(version, "monstats.txt")
     treasureclassex = get_base_file(version, "treasureclassex.txt")
@@ -23,10 +24,21 @@ def get(version):
 
     df = df.loc[df['Id'].isin(boss_ids_to_keep)]
 
+    # df['TC'] = df.apply(get_treasure_class_for_difficulty, args=(
+    #     treasureclassex, 'TreasureClass3',), axis=1)
+    # df['TC(N)'] = df.apply(get_treasure_class_for_difficulty,
+    #                        args=(treasureclassex, 'TreasureClass3(N)',), axis=1)
+    # df['TC(H)'] = df.apply(get_treasure_class_for_difficulty,
+    #                        args=(treasureclassex, 'TreasureClass3(H)',), axis=1)
+
     # Get all the TC items each can drop
-    df['TC'] = df.apply(get_treasure_class_for_difficulty, args=(treasureclassex, 'TreasureClass3',), axis=1)
-    df['TC(N)'] = df.apply(get_treasure_class_for_difficulty, args=(treasureclassex, 'TreasureClass3(N)',), axis=1)
-    df['TC(H)'] = df.apply(get_treasure_class_for_difficulty, args=(treasureclassex, 'TreasureClass3(H)',), axis=1)
+    # Sometime around v2.7.0 they changed the column to "TreasureClassUnique"
+    df['TC'] = df.apply(get_treasure_class_for_difficulty, args=(
+        treasureclassex, 'TreasureClassUnique',), axis=1)
+    df['TC(N)'] = df.apply(get_treasure_class_for_difficulty,
+                           args=(treasureclassex, 'TreasureClassUnique(N)',), axis=1)
+    df['TC(H)'] = df.apply(get_treasure_class_for_difficulty,
+                           args=(treasureclassex, 'TreasureClassUnique(H)',), axis=1)
 
     # Change bosses NameStr's to be a bit more user friendly
     id_to_namestr_mapping = {
@@ -42,17 +54,19 @@ def get(version):
         'uberbaal': 'Uber Baal',
     }
 
-    df['NameStr'] = df['Id'].apply(lambda Id: id_to_namestr_mapping[Id] if Id in id_to_namestr_mapping.keys() else Id)
+    df['NameStr'] = df['Id'].apply(
+        lambda Id: id_to_namestr_mapping[Id] if Id in id_to_namestr_mapping.keys() else Id)
 
     # We don't want diablo clone showing results for normal and nightmare
     # It might be possible for him to spawn in those difficulties, but it's visual clutter
     # and confusing for users
     # We're going to just set their level on Normal and NM to a very low value
     # So it won't pass the high enough level check
-    df.at[df['Id'] == 'diabloclone', 'Level'] = -9999
-    df.at[df['Id'] == 'diabloclone', 'Level(N)'] = -9999
+    df.loc[df['Id'] == 'diabloclone', 'Level'] = -9999
+    df.loc[df['Id'] == 'diabloclone', 'Level(N)'] = -9999
 
     # Save for debugging
-    df.to_json(path.join(get_generated_dir(), "monstats.json"), orient='records', indent=2)
+    df.to_json(path.join(get_generated_dir(), "monstats.json"),
+               orient='records', indent=2)
 
     return df[['Id', 'NameStr', 'Level', 'Level(N)', 'Level(H)', 'boss', 'TC', 'TC(N)', 'TC(H)']]
--- d2r/newutil/uniqueitems.py
+++ d2r/newutil/uniqueitems.py
@@ -32,31 +32,40 @@ def get(version):
     #
 
     # Lookup their base items levels
-    df['base_item_level'] = df.apply(calc_base_item_level, args=(weapons,), axis=1)
-    df['base_item_level'] = df.apply(calc_base_item_level, args=(armor,), axis=1)
-    df['base_item_level'] = df.apply(calc_base_item_level, args=(misc,), axis=1)
+    df['base_item_level'] = df.apply(
+        calc_base_item_level, args=(weapons,), axis=1)
+    df['base_item_level'] = df.apply(
+        calc_base_item_level, args=(armor,), axis=1)
+    df['base_item_level'] = df.apply(
+        calc_base_item_level, args=(misc,), axis=1)
 
-    assert df['base_item_level'].isnull().sum() == 0, f"One or more of the `base_item_levels` are null"
+    assert df['base_item_level'].isnull().sum(
+    ) == 0, f"One or more of the `base_item_levels` are null"
 
     # Lookup their base items types
-    df['base_item_type'] = df.apply(calc_base_item_type, args=(weapons, "weap"), axis=1)
-    df['base_item_type'] = df.apply(calc_base_item_type, args=(armor, "armo"), axis=1)
-    df['base_item_type'] = df.apply(calc_base_item_type, args=(misc, 'misc'), axis=1)
+    df['base_item_type'] = df.apply(
+        calc_base_item_type, args=(weapons, "weap"), axis=1)
+    df['base_item_type'] = df.apply(
+        calc_base_item_type, args=(armor, "armo"), axis=1)
+    df['base_item_type'] = df.apply(
+        calc_base_item_type, args=(misc, 'misc'), axis=1)
 
-    assert df['base_item_type'].isnull().sum() == 0, f"One or more of the `base_item_types` are null"
+    assert df['base_item_type'].isnull().sum(
+    ) == 0, f"One or more of the `base_item_types` are null"
 
     # Assign their base TC class
     df['tc_group'] = df.apply(calc_tc_group, axis=1)
 
     # Hack Hack Hack
     # Annihilus is a 'cm1' code but can only be dropped by dclone
-    # Change it's group so it'll be found in dclone's dropped tcs (which reference it by 'Annihilus' in TreasureClassEx.txt)
-    df.at[df['index'] == 'Annihilus', 'tc_group'] = 'Annihilus'
+    # Change its group so it'll be found in dclone's dropped tcs (which reference it by 'Annihilus' in TreasureClassEx.txt)
+    df.loc[df['index'] == "Annihilus", "tc_group"] = 'Annihilus'
 
     # 'Rainbow Facet' - shows up multiple times, they're all the same level with the same code, remove them
     df.drop_duplicates(subset="index", inplace=True)
 
     # Save for debugging
-    df.to_json(path.join(get_generated_dir(), "uniqueitems.json"), orient='records', indent=2)
+    df.to_json(path.join(get_generated_dir(), "uniqueitems.json"),
+               orient='records', indent=2)
 
     return df[['index', 'lvl', 'rarity_type', 'tc_group']]
